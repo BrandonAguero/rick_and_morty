c59817eb443c11288d71d3c914aabf74588c675d
Brandon <name.brandon.aguero@gmail.com>
2023-06-03 10:02:26 -0500

    I added a search feature by name.

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,23 +1,45 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './styles/InputSearch.css';
+import useFetch from '../hooks/useFetch';
 
 const InputSearch = () => {
 
-    const valueInput = useRef();
-    const [inputValue, setInputValue] = useState()
+    const url = 'https://rickandmortyapi.com/api/location';
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        setInputValue(valueInput.current.value.trim())
-        valueInput.current.value = '';
+    const [allLocations, getAllLocations] = useFetch(url)
+
+    useEffect(() => {
+        getAllLocations()
+    }, [])
+    
+    console.log(allLocations?.results)
+
+    const valueInput = useRef();
+    const [inputValue, setInputValue] = useState([])
+    const nameLocationAll =  allLocations?.results.map(location => location.name)
+
+    const handleChangeInput = () => {
+        const nameLocation = valueInput.current.value.trim()
+        const nameEqualLocation = nameLocationAll.filter((name) => name.includes(nameLocation))
+        if (nameEqualLocation.length !== 20) setInputValue(nameEqualLocation)
+        if (nameEqualLocation.length === 20) setInputValue([])
     }
 
     console.log(inputValue)    
 
     return (
-        <form className="main__form" onSubmit={handleClick}>
-            <input placeholder="Enter a name location" type='text' ref={valueInput}/>
-            <button>Search</button>
+        <form className="main__form" onChange={handleChangeInput}>
+            <div>
+                <input placeholder="Enter a name location" type='text' ref={valueInput}/>
+                <button>Search</button>
+            </div>
+            <div className={`${inputValue.length ? 'div' : 'notDiv'}`}>
+                <ul>
+                    {
+                        inputValue?.map((name) => (<li key={name}>{name}</li> ))
+                    }
+                </ul>
+            </div>
         </form>
     )
 }
